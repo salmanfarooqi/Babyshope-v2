3f8ae5826a1b6a1c1754ccd5ea5a6bab81b692dd
SalmanKhanOfficial <afaqhussain098@gmail.com>
2024-04-03 06:21:05 +0500

    afaq

--- vite-project/src/components/CouponCode.jsx
+++ vite-project/src/components/CouponCode.jsx
@@ -6,10 +6,8 @@ function CouponCode(className) {
     <div className='w-full flex flex-col justify-center items-center'>
     <label className=''>Enter your coupon code if you have one</label>
     <div className='mt-5 flex gap-2'>
-        <input type='text' className='border border-gray-500 py-3 px-2  rounded-md' placeholder='Coupon Code'/>
-        <button className='px-4  rounded-3xl bg-[#222222] font-semibold text-white'>Apply</button>
+        <input type='text' className='border border-gray-500 py-3 px-2  rounded-md' placeholder='Coupon'/>
     </div>
-
     </div>
       
     </div>
